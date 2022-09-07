841dc317cbe189b9c3d31ee1c8e944b8fbf6b3a9
Femiloye Oyerinde <100911418+phemiloyeAI@users.noreply.github.com>
2022-09-07 14:53:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import cv2
 import time
 import json
 import tempfile
-import shutil
 import numpy as np
 
 from utils import choose_weights, DFC_inference, get_dfc_crop_label, extract_HSV_mask, CropCoverageArea
@@ -16,7 +15,6 @@ app = FastAPI()
 def home():
     return {"Inference on DFC Model"}
 
-@app.get("/")
 def download_file(name_file: str, media_type):
     return FileResponse(path=name_file, media_type=media_type, filename=name_file)
 
@@ -75,17 +73,20 @@ def create_upload_file(crop_stage: int, file: UploadFile=File(..., )):
     info_dst = os.path.join(os.getcwd(), "info.json")
     info["Output dst"] = dst
 
+    msg = f"Crop coverage area: {crop_area}%"
+    text_size = cv2.getTextSize(msg, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+    text_w, text_h = text_size[0]; x, y = (14, 10)
+    cv2.rectangle(output, (x, y), (x+text_w+21, y+text_h+20), (255, 255, 255), -1); y = y+text_h+30 // 2
+    cv2.putText(output, msg, (x+5, y-5), cv2.FONT_HERSHEY_COMPLEX, fontScale=0.5, color=(255, 0, 0), thickness=1, lineType=cv2.LINE_AA)
+
 #     # save to disk
     cv2.imwrite(dst, output)
+
     with open(info_dst, "w") as fp:
          json.dump(info, fp, indent=2)
-    
-#     download to locally
-    download_file(name_file=dst, media_type="image/jpeg") # segmentation image
-    download_file(name_file=info_dst, media_type="info/json") # info json file
-  
+
     end = time.time()
     duration = round(end-start, 3)
     print(f"It takes {duration} seconds to make the prediction.")
 
-    return info
+    return download_file(name_file=dst, media_type="image/jpeg")
